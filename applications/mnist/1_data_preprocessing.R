7ac1a16447b197a638be319dbc578b9a2debc7f0
JayaniLakshika <jayanilakshika76@gmail.com>
2024-03-11 12:23:09 +1100

    Added Handwritten digit: 1 dataset

---
+++ applications/mnist/1_data_preprocessing.R
@@ -0,0 +1,47 @@
+library(readr)
+library(reticulate)
+library(dplyr)
+library(ggplot2)
+
+mnist_data <- read_rds("~/Desktop/PhD Monash research files/paper-nldr-vis-algorithm/data/mnist/mnist_digit_1.rds")
+
+data_x <- mnist_data[,-785]/255
+data_x <- array_reshape(data_x, c(nrow(data_x), 784), order = "F")
+data_x_df <- as.data.frame(data_x)
+names(data_x_df) <- paste0(rep("x", ncol(data_x_df)), 1:ncol(data_x_df))
+
+# PCA
+
+data_x_df <- data_x_df |>
+  select(where(~ any(. != 0)))
+
+dim(data_x_df)
+
+
+#Spectral decomposition which examines the covariances / correlations between variables
+#princomp(data, cor = FALSE)
+calculate_pca <- function(feature_dataset, num_pcs){
+  pcaY_cal <- prcomp(feature_dataset, center = TRUE, scale = TRUE)
+  PCAresults <- data.frame(pcaY_cal$x[, 1:num_pcs])
+  summary_pca <- summary(pcaY_cal)
+  var_explained_df <- data.frame(PC= paste0("PC",1:50),
+                                 var_explained=(pcaY_cal$sdev[1:50])^2/sum((pcaY_cal$sdev[1:50])^2))
+  return(list(prcomp_out = pcaY_cal,pca_components = PCAresults, summary = summary_pca, var_explained_pca  = var_explained_df))
+}
+
+pca_ref_calc <- calculate_pca(data_x_df,10)
+pca_ref_calc$summary
+var_explained_df <- pca_ref_calc$var_explained_pca
+data_pca <- pca_ref_calc$pca_components
+
+var_explained_df %>%
+  ggplot(aes(x = PC,y = var_explained, group = 1))+
+  geom_point(size=1)+
+  geom_line()+
+  labs(title="Scree plot: PCA on scaled data") +
+  scale_x_discrete(limits = paste0(rep("PC", 50), 1:50)) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+write_rds(data_pca, "data/mnist/mnist_10_pcs_of_digit_1.rds")
+
+
---
+++ applications/mnist/2_gen_pacmapR.R
@@ -0,0 +1,45 @@
+library(dplyr)
+library(snedata)
+library(ggflowchart)
+library(purrr) ## map function
+library(gridExtra) ## for grid.arrange
+library(rsample)
+library(DT)
+library(ggbeeswarm)
+library(ggplot2)
+library(readr)
+
+library(reticulate)
+
+
+library(grid)
+
+set.seed(20240110)
+
+use_python("~/miniforge3/envs/pcamp_env/bin/python")
+use_condaenv("pcamp_env")
+
+reticulate::source_python(paste0(here::here(), "/examples/function_scripts/Fit_PacMAP_code.py"))
+reticulate::source_python(paste0(here::here(), "/examples/function_scripts/Fit_TriMAP_code.py"))
+
+source("nldr_code.R", local = TRUE)
+
+mnist_10_pcs_of_digit_4 <- readRDS("data/mnist/mnist_10_pcs_of_digit_1.rds")
+
+## PaCMAP
+
+tem_dir <- tempdir()
+
+Fit_PacMAP_data(mnist_10_pcs_of_digit_4, tem_dir)
+
+path <- file.path(tem_dir, "df_2_without_class.csv")
+path2 <- file.path(tem_dir, "dataset_3_PaCMAP_values.csv")
+
+Fit_PaCMAP(as.integer(2), as.integer(10), "random", 0.9, as.integer(2), path, path2)
+
+PacMAP_data <- read_csv(path2)
+PacMAP_data <- PacMAP_data |>
+  mutate(ID = 1:NROW(PacMAP_data))
+
+write_rds(PacMAP_data, file = "data/mnist/mnist_pacmap.rds")
+
