859b30bbd607c5d6113c824996b6b83c8fd79f60
JayaniLakshika <jayanilakshika76@gmail.com>
2024-06-18 08:28:25 +1000

    Updated the screenshots of MNIST and PBMC

--- examples/scurve/8_gen_best_model_in_data_space.R
+++ examples/scurve/8_gen_best_model_in_data_space.R
@@ -518,6 +518,6 @@ df <- dplyr::bind_rows(model_df, training_data |> mutate(type = "data"))
 
 langevitour(df |> dplyr::select(-type), lineFrom = point_connect_all$from,
             lineTo = point_connect_all$to,
-            group = df$type, pointSize = append(rep(1, NROW(true_model)), rep(3, NROW(df_b) + NROW(training_data))), levelColors = c("#6a3d9a", "#33a02c", "#d95f02"),
-            lineColors = append(rep("#e41a1c", length(append(point_connect_df$from, point_connect_df_dir_2$from))), rep("black", length(distance_df_small_edges$from))))
+            group = df$type, pointSize = append(rep(1, NROW(true_model) +  NROW(df_b)), rep(0.5, NROW(training_data))), levelColors = c("#6a3d9a", "#33a02c", "#969696"),
+            lineColors = append(rep("#969696", length(append(point_connect_df$from, point_connect_df_dir_2$from))), rep("black", length(distance_df_small_edges$from))))
 
