8ca1e0366acd491a80db9a6576dff356d269ed4f
JayaniLakshika <jayanilakshika76@gmail.com>
2024-06-30 13:43:02 +1000

    Update figure for prediction

--- examples/scurve/9_gen_prediction_true_model_with_umap.R
+++ examples/scurve/9_gen_prediction_true_model_with_umap.R
@@ -2,51 +2,40 @@ library(readr)
 library(umap)
 library(dplyr)
 
-true_model <- read_rds("data/s_curve/true_model.rds") |>
+true_model_df <- read_rds("data/s_curve/true_model.rds")
+true_model_df <- true_model_df |>
   mutate(ID = row_number())
 
-# ### UMAP
-#
-# UMAP_fit <- umap(true_model |> select(-ID), n_neighbors = 15, n_components =  2)
-#
-# UMAP_data_gau <- UMAP_fit$layout |>
-#   as.data.frame()
-# names(UMAP_data_gau)[1:(ncol(UMAP_data_gau))] <- paste0(rep("UMAP",(ncol(UMAP_data_gau))), 1:(ncol(UMAP_data_gau)))
-#
-# UMAP_data_gau <- UMAP_data_gau |>
-#   mutate(ID = row_number())
-#
-# write_rds(UMAP_data_gau, file = "data/s_curve/umap_data_true_model.rds")
-#
-# scaled_umap <- gen_scaled_data(data = UMAP_data_gau)
-#
-# s_curve_noise_umap_scaled <- scaled_umap$scaled_nldr
-# lim1 <- scaled_umap$lim1
-# lim2 <- scaled_umap$lim2
-# r2 <- diff(lim2)/diff(lim1)
+training_data_scurve <- read_rds("data/s_curve/s_curve_training.rds")
 
-# model <- fit_highd_model(training_data = true_model,
-#                          emb_df = s_curve_noise_umap_scaled,
-#                          bin1 = 12, r2 = r2,
-#                          col_start_highd = "x")
-# df_bin_centroids <- model$df_bin_centroids
-# df_bin <- model$df_bin
+## To initialize effective bins along x
+effective_bin1_scurve <- 12
 
-pred_df_training <- predict_emb(test_data = true_model,
-                             df_bin_centroids = df_bin_centroids_scurve,
-                             df_bin = df_bin_scurve, type_NLDR = "UMAP")
-glimpse(pred_df_training)
+scurve_model <- fit_highd_model(
+  training_data = training_data_scurve,
+  emb_df = umap_scurve_scaled,
+  bin1 = effective_bin1_scurve,
+  r2 = r2,
+  q = 0.07,
+  is_bin_centroid = TRUE,
+  is_rm_lwd_hex = FALSE,
+  col_start_highd = "x"
+)
 
-s_curve_noise_umap_scaled |>
-    ggplot(aes(x = UMAP1,
-               y = UMAP2,
-               label = ID))+
-    geom_point(alpha=0.5) +
-    geom_point(data = pred_df_training, aes(x = pred_UMAP_1, y = pred_UMAP_2),
-               color = "red", alpha=0.5) +
-    coord_equal() +
-    theme(plot.title = element_text(hjust = 0.5, size = 18, face = "bold"),
-          axis.text = element_text(size = 5),
-          axis.title = element_text(size = 7))
+df_bin_centroids_scurve <- scurve_model$df_bin_centroids
+df_bin_scurve <- scurve_model$df_bin
 
+true_pred_df <- predict_emb(
+  test_data = true_model_df,
+  df_bin_centroids = df_bin_centroids_scurve,
+  df_bin = df_bin_scurve,
+  type_NLDR = "UMAP"
+)
 
+true_pred_df |>
+  ggplot(aes(x = pred_UMAP_1,
+             y = pred_UMAP_2)) +
+  geom_point(
+    alpha=0.5,
+    color = "#33a02c"
+  )
