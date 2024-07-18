1096ed866c3bd48d8d5b71c793b3d670f1a48d44
JayaniLakshika <jayanilakshika76@gmail.com>
2024-07-18 20:25:00 +1000

    Update prediction

--- examples/scurve/9_gen_prediction_true_model_with_umap.R
+++ examples/scurve/9_gen_prediction_true_model_with_umap.R
@@ -9,7 +9,7 @@ true_model_df <- true_model_df |>
 training_data_scurve <- read_rds("data/s_curve/s_curve_training.rds")
 
 ## To initialize effective bins along x
-effective_bin1_scurve <- 12
+effective_bin1_scurve <- 11
 
 scurve_model <- fit_highd_model(
   training_data = training_data_scurve,
@@ -32,10 +32,43 @@ true_pred_df <- predict_emb(
   type_NLDR = "UMAP"
 )
 
-true_pred_df |>
-  ggplot(aes(x = pred_UMAP_1,
-             y = pred_UMAP_2)) +
-  geom_point(
+## Compute hexbin parameters
+num_bins_x_scurve <- 11
+
+## hexagon binning to have regular hexagons
+hb_obj_scurve <- hex_binning(
+  data = umap_scurve_scaled,
+  bin1 = num_bins_x_scurve,
+  r2 = r2,
+  q = 0.07)
+
+## Data set with all centroids
+all_centroids_df <- hb_obj_scurve$centroids
+
+## Generate all coordinates of hexagons
+hex_grid <- hb_obj_scurve$hex_poly
+
+## To obtain the standardise counts within hexbins
+counts_df <- hb_obj_scurve$std_cts
+
+hex_grid_with_counts <-
+  left_join(hex_grid,
+            counts_df,
+            by = c("hex_poly_id" = "hb_id"))
+
+hex_grid_nonempty <- hex_grid |>
+  filter(hex_poly_id %in% df_bin_centroids_scurve$hexID)
+
+ggplot(
+  data = hex_grid_nonempty,
+  aes(x = x, y = y)) +
+  geom_polygon(color = "black",
+               aes(group = hex_poly_id),
+               fill = "#ffffff") +
+  geom_jitter(
+    data = true_pred_df,
+    aes(x = pred_UMAP_1,
+        y = pred_UMAP_2),
     alpha=0.5,
-    color = "#33a02c"
+    color = "#969696"
   )
