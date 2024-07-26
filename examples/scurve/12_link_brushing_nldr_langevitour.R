fe339ec5ed92ade9ba0f7406c564e79536bfabb7
JayaniLakshika <jayanilakshika76@gmail.com>
2024-07-26 13:16:22 +1000

    add reset with NLDR plot with double click, fix the theme and sizes of the plots

--- examples/scurve/12_link_brushing_nldr_langevitour.R
+++ examples/scurve/12_link_brushing_nldr_langevitour.R
@@ -1,5 +1,8 @@
 library(crosstalk)
 library(plotly)
+library(readr)
+library(quollr)
+library(langevitour)
 
 umap_scurve <- read_rds(file = "data/s_curve/s_curve_umap.rds")
 
@@ -14,30 +17,46 @@ r2 <- diff(lim2)/diff(lim1)
 df_all_scurve <- dplyr::bind_cols(training_data_scurve |> dplyr::select(-ID),
                                   umap_data_with_hb_id)
 
-# ### Define type column
-# df <- df_all_scurve |>
-#   dplyr::select(tidyselect::starts_with("x")) |>
-#   dplyr::mutate(type = "data") ## original dataset
-#
 # df_b <- df_bin_scurve |>
 #   dplyr::filter(hb_id %in% df_bin_centroids_scurve$hexID) |>
 #   dplyr::mutate(type = "model") ## Data with summarized mean
 #
 # ## Reorder the rows of df_b according to the hexID order in df_b_with_center_data
-# df_b <- df_b[match(df_bin_centroids_scurve$hexID, df_b$hb_id),] |>
+# df_b_n <- df_b[match(df_bin_centroids_scurve$hexID, df_b$hb_id),] |>
 #   select(-type)
 #
-# names(df_b)[2:NCOL(df_b)] <- paste0("avg_", names(df_b)[2:NCOL(df_b)])
-#
-# df_all_scurve <- left_join(df_all_scurve, df_b, by = "hb_id")
+# df_b_n <- left_join(df_b_n, df_bin_centroids_scurve, by = c("hb_id" = "hexID")) |>
+#   select(-hb_id, -std_counts, -drop_empty)
 
-shared_df_scurve <- SharedData$new(df_all_scurve |> select(-ID, -hb_id))
+df_all_scurve_n <- df_all_scurve |>
+  select(-ID, -hb_id)
+
+# names(df_b_n) <- names(df_all_scurve_n)
+
+shared_df_scurve <- SharedData$new(df_all_scurve_n)
 
 nldr_scurve <- shared_df_scurve |>
   ggplot(aes(x = UMAP1, y = UMAP2)) +
-  geom_point(alpha=0.5, colour="#6a3d9a", size = 0.5)
+  geom_point(alpha=0.5, colour="#6a3d9a", size = 0.5) +
+  theme_linedraw() +
+  theme(
+    #aspect.ratio = 1,
+    plot.background = element_rect(fill = 'transparent', colour = NA),
+    plot.title = element_text(size = 7, hjust = 0.5, vjust = -0.5),
+    panel.background = element_rect(fill = 'transparent',
+                                    colour = NA),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.title.x = element_blank(), axis.title.y = element_blank(),
+    axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+    axis.text.y = element_blank(), axis.ticks.y = element_blank()
+  )
 
-nldr_scurve_plt <- ggplotly(nldr_scurve)
+nldr_scurve_plt <- ggplotly(nldr_scurve, width = as.character(round(600/r2, 0)),
+                            height = "600") |>
+  style(unselected=list(marker=list(opacity=1))) |>
+  highlight(on="plotly_selected", off="plotly_deselect") |>
+  config(displayModeBar = FALSE)
 
 langevitour_output <- langevitour(training_data |> select(-type),
                                   link=shared_df_scurve)
