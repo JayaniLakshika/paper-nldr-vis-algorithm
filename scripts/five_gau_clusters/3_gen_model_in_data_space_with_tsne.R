793c8b42dde907c21cdef4c0b4e5b1997824c478
JayaniLakshika <jayanilakshika76@gmail.com>
2024-08-22 20:58:11 +1000

    Need to fix later

--- scripts/five_gau_clusters/3_gen_model_in_data_space_with_tsne.R
+++ scripts/five_gau_clusters/3_gen_model_in_data_space_with_tsne.R
@@ -14,7 +14,7 @@ gau1_scaled_obj <- gen_scaled_data(
 tsne_gau_scaled <- gau1_scaled_obj$scaled_nldr
 
 ## Compute hexbin parameters
-num_bins_x_gau1 <- 14
+num_bins_x_gau1 <- 13
 lim1 <- gau1_scaled_obj$lim1
 lim2 <- gau1_scaled_obj$lim2
 r2_gau1 <- diff(lim2)/diff(lim1)
@@ -27,7 +27,7 @@ gau1_model <- fit_highd_model(
   is_bin_centroid = TRUE,
   is_rm_lwd_hex = FALSE,
   col_start_highd = "x",
-  q = 0.16
+  q = 0.1
 )
 
 df_bin_centroids_gau1 <- gau1_model$df_bin_centroids
@@ -43,9 +43,9 @@ tr_from_to_df_gau1 <- gen_edges(
 #   filter(row_number() != 76) |>
 #   filter(row_number() != 34)
 
-tr_from_to_df_gau1 <- tr_from_to_df_gau1 |>
-  filter(row_number() != 115) |>
-  filter(row_number() != 139)
+# tr_from_to_df_gau1 <- tr_from_to_df_gau1 |>
+#   filter(row_number() != 115) |>
+#   filter(row_number() != 139)
 
 ## Compute 2D distances
 distance_gau1 <- cal_2d_dist(
@@ -62,7 +62,6 @@ benchmark_gau1 <- find_lg_benchmark(
   distance_col = "distance")
 
 
-
 trimesh_removed_gau1 <- vis_rmlg_mesh(
   distance_edges = distance_gau1,
   benchmark_value = benchmark_gau1,
@@ -75,7 +74,7 @@ hb_obj_gau1 <- hex_binning(
   data = tsne_gau_scaled,
   bin1 = num_bins_x_gau1,
   r2 = r2_gau1,
-  q = 0.16)
+  q = 0.1)
 
 tsne_data_with_hb_id <- hb_obj_gau1$data_hb_id
 
