6f47f48303690fc3d78ef0b6fba686b24a7303e2
JayaniLakshika <jayanilakshika76@gmail.com>
2024-08-29 12:17:07 +1000

    Generate embeddings with defaults for Five Gaussian clusters

--- scripts/five_gau_clusters/1_five_gaussian_cluster_data_emb.R
+++ scripts/five_gau_clusters/1_five_gaussian_cluster_data_emb.R
@@ -1,11 +1,6 @@
 library(dplyr)
-library(snedata)
-library(ggflowchart)
 library(purrr) ## map function
-library(gridExtra) ## for grid.arrange
 library(rsample)
-library(DT)
-library(ggbeeswarm)
 library(ggplot2)
 library(readr)
 
@@ -72,37 +67,38 @@ df_2n <- df_2n |>
 write_rds(df_2n, file = "data/five_gau_clusters/data_five_gau_with_labels.rds")
 
 
-data_split_sp <- initial_split(df_2)
-training_data_5 <- training(data_split_sp) |>
-  arrange(ID)
-test_data_5 <- testing(data_split_sp) |>
-  arrange(ID)
-
-write_rds(training_data_5, file = "data/five_gau_clusters/data_five_gau_training.rds")
-write_rds(test_data_5, file = "data/five_gau_clusters/data_five_gau_test.rds")
+# data_split_sp <- initial_split(df_2)
+# training_data_5 <- training(data_split_sp) |>
+#   arrange(ID)
+# test_data_5 <- testing(data_split_sp) |>
+#   arrange(ID)
+#
+# write_rds(training_data_5, file = "data/five_gau_clusters/data_five_gau_training.rds")
+# write_rds(test_data_5, file = "data/five_gau_clusters/data_five_gau_test.rds")
 
 ### tSNE
-tSNE_data_gau <- Fit_tSNE(training_data_5 |> dplyr::select(-ID), opt_perplexity = calculate_effective_perplexity(training_data_5), with_seed = 20240110)
+tSNE_data_gau <- Fit_tSNE(df_2 |> dplyr::select(-ID), opt_perplexity = 30, with_seed = 20240110)
 
 tSNE_data_gau <- tSNE_data_gau |>
   select(-ID) |>
-  mutate(ID = training_data_5$ID)
+  mutate(ID = df_2$ID)
 
 plot_tSNE_2D(tSNE_data_gau)
 
-write_rds(tSNE_data_gau, file = paste0("data/five_gau_clusters/tsne_data_five_gau_", calculate_effective_perplexity(training_data_5),".rds"))
+#write_rds(tSNE_data_gau, file = paste0("data/five_gau_clusters/tsne_data_five_gau_", calculate_effective_perplexity(df_2),".rds"))
+write_rds(tSNE_data_gau, file = paste0("data/five_gau_clusters/tsne_data_five_gau_30.rds"))
 
 
 ### UMAP
 
-UMAP_fit <- umap(training_data_5 |> dplyr::select(-ID), n_neighbors = 15, n_components =  2)
+UMAP_fit <- umap(df_2 |> dplyr::select(-ID), n_neighbors = 15, n_components =  2)
 
 UMAP_data_gau <- UMAP_fit$layout |>
   as.data.frame()
 names(UMAP_data_gau)[1:(ncol(UMAP_data_gau))] <- paste0(rep("UMAP",(ncol(UMAP_data_gau))), 1:(ncol(UMAP_data_gau)))
 
 UMAP_data_gau <- UMAP_data_gau |>
-  mutate(ID = training_data_5$ID)
+  mutate(ID = df_2$ID)
 
 write_rds(UMAP_data_gau, file = "data/five_gau_clusters/umap_data_five_gau.rds")
 
@@ -125,16 +121,17 @@ write_rds(predict_UMAP_df, file = "data/five_gau_clusters/umap_data_five_gau_pre
 
 tem_dir <- tempdir()
 
-Fit_TriMAP_data(training_data_5 |> dplyr::select(-ID), tem_dir)
+Fit_TriMAP_data(df_2 |> dplyr::select(-ID), tem_dir)
 
 path <- file.path(tem_dir, "df_2_without_class.csv")
 path2 <- file.path(tem_dir, "dataset_3_TriMAP_values.csv")
 
-Fit_TriMAP(as.integer(2), as.integer(5), as.integer(4), as.integer(3), path, path2)
+#Fit_TriMAP(as.integer(2), as.integer(5), as.integer(4), as.integer(3), path, path2)
+Fit_TriMAP(as.integer(2), as.integer(12), as.integer(4), as.integer(3), path, path2)
 
 TriMAP_data <- read_csv(path2)
 TriMAP_data <- TriMAP_data |>
-  mutate(ID = training_data_5$ID)
+  mutate(ID = df_2$ID)
 write_rds(TriMAP_data, file = "data/five_gau_clusters/trimap_data_five_gau.rds")
 
 
@@ -142,16 +139,17 @@ write_rds(TriMAP_data, file = "data/five_gau_clusters/trimap_data_five_gau.rds")
 
 tem_dir <- tempdir()
 
-Fit_PacMAP_data(training_data_5 |> dplyr::select(-ID), tem_dir)
+Fit_PacMAP_data(df_2 |> dplyr::select(-ID), tem_dir)
 
 path <- file.path(tem_dir, "df_2_without_class.csv")
 path2 <- file.path(tem_dir, "dataset_3_PaCMAP_values.csv")
 
-Fit_PaCMAP(as.integer(2), as.integer(10), "random", 0.9, as.integer(2), path, path2)
+# Fit_PaCMAP(as.integer(2), as.integer(10), "random", 0.9, as.integer(2), path, path2)
+Fit_PaCMAP(as.integer(2), as.integer(10), "random", 0.5, as.integer(2), path, path2)
 
 PacMAP_data <- read_csv(path2)
 PacMAP_data <- PacMAP_data |>
-  mutate(ID = training_data_5$ID)
+  mutate(ID = df_2$ID)
 
 write_rds(PacMAP_data, file = "data/five_gau_clusters/pacmap_data_five_gau.rds")
 
@@ -159,11 +157,11 @@ write_rds(PacMAP_data, file = "data/five_gau_clusters/pacmap_data_five_gau.rds")
 
 ### Phate
 
-PHATE_data <- Fit_PHATE(training_data_5 |> dplyr::select(-ID), knn = 5, with_seed = 20240110)
+PHATE_data <- Fit_PHATE(df_2 |> dplyr::select(-ID), knn = 5, with_seed = 20240110)
 PHATE_data <- PHATE_data |>
   select(PHATE1, PHATE2)
 PHATE_data <- PHATE_data |>
-  mutate(ID = training_data_5$ID)
+  mutate(ID = df_2$ID)
 
 write_rds(PHATE_data, file = "data/five_gau_clusters/phate_data_five_gau.rds")
 
