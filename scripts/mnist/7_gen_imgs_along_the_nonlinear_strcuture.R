b3225b34a99d8d08568eef6c536882db0345ccbb
JayaniLakshika <jayanilakshika76@gmail.com>
2024-09-05 02:19:11 +1000

    Added images on the tSNE layout (MNIST)

--- scripts/mnist/7_gen_imgs_along_the_nonlinear_strcuture.R
+++ scripts/mnist/7_gen_imgs_along_the_nonlinear_strcuture.R
@@ -1,3 +1,7 @@
+library(readr)
+library(dplyr)
+library(tidyr)
+
 mnist_data <- read_rds("data/mnist/mnist_digit_1.rds")
 
 img_right_top <- c(6561, 454, 6397)
@@ -7,7 +11,7 @@ pixels_gathered_within <-  mnist_data |>
   gather(pixel, value, -Label, -instance) |>
   extract(pixel, "pixel", "(\\d+)", convert = TRUE) |>
   mutate(pixel = pixel - 2, x = pixel %% 28, y = 28 - pixel %/% 28) |>
-  filter(instance %in% img_error_within)
+  filter(instance %in% img_right_top)
 
 right_top_img <- pixels_gathered_within |>
   ggplot(aes(x, y, fill = value)) +
@@ -19,14 +23,14 @@ right_top_img <- pixels_gathered_within |>
         strip.text.x = element_blank(),
         legend.position = "none")
 
-img_middle <- c(4671, 3444, 3694)
+img_middle <- c(3443, 7404, 7242)
 
 pixels_gathered_within <-  mnist_data |>
   mutate(instance = row_number()) |>
   gather(pixel, value, -Label, -instance) |>
   extract(pixel, "pixel", "(\\d+)", convert = TRUE) |>
   mutate(pixel = pixel - 2, x = pixel %% 28, y = 28 - pixel %/% 28) |>
-  filter(instance %in% img_error_within)
+  filter(instance %in% img_middle)
 
 middle_img <- pixels_gathered_within |>
   ggplot(aes(x, y, fill = value)) +
@@ -38,14 +42,14 @@ middle_img <- pixels_gathered_within |>
         strip.text.x = element_blank(),
         legend.position = "none")
 
-img_right_bottom <- c(296, 6386, 2)
+img_right_bottom <- c(7098, 6456, 2)
 
 pixels_gathered_within <-  mnist_data |>
   mutate(instance = row_number()) |>
   gather(pixel, value, -Label, -instance) |>
   extract(pixel, "pixel", "(\\d+)", convert = TRUE) |>
   mutate(pixel = pixel - 2, x = pixel %% 28, y = 28 - pixel %/% 28) |>
-  filter(instance %in% img_error_within)
+  filter(instance %in% img_right_bottom)
 
 right_bottom_img <- pixels_gathered_within |>
   ggplot(aes(x, y, fill = value)) +
