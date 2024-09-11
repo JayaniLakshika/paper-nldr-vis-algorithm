517c6a5a198ec795dbd740bb289cb811b0fc7a58
JayaniLakshika <jayanilakshika76@gmail.com>
2024-09-11 22:19:06 +1000

    Added new data structure to replace the S-curve

--- scripts/two_curvilinear_clusters/1_gen_data.R
+++ scripts/two_curvilinear_clusters/1_gen_data.R
@@ -1,28 +1,33 @@
+library(dplyr)
+
+set.seed(20240110)
+
 # Function to generate a 2D curvilinear cluster with noise in the other dimensions
-generate_curvilinear_2d_with_noise <- function(n, noise_dim1, noise_dim2) {
-  x <- runif(n, 0, 2)
-  y <- -(x^3 + runif(n, 0, 3)) + runif(n, 0, 0.5)
+generate_curvilinear_2d_with_noise <- function(n) {
+  x1 <- runif(n, 0, 2)
+  x2 <- -(x1^3 + runif(n, 0, 3)) + runif(n, 0, 0.5)
 
   # Add small noise to other dimensions
-  z_noise <- rnorm(n, mean = 0, sd = noise_dim1)
-  w_noise <- rnorm(n, mean = 0, sd = noise_dim2)
+  x3 <- sin(x1 * pi) + runif(n, -0.5, 0.5)
+  x4 <- rnorm(n, mean = 0, sd = 0.01)
 
-  return(cbind(x, y, z_noise, w_noise))
+  data <- tibble::tibble(x1 = x1,
+                         x2 = x2,
+                         x3 = x3,
+                         x4 = x4)
+
+  data
 }
 
 # Number of points for each cluster
 n_points <- 500
 
 # Generate the first curvilinear cluster in dimensions 1 and 2 with small noise in dimensions 3 and 4
-curv_1_2 <- generate_curvilinear_2d_with_noise(n_points, noise_dim1 = 0.01, noise_dim2 = 0.01) |>
-  as_tibble()
-
-names(curv_1_2) <- c("x1", "x2", "x3", "x4")
+curv_1_2 <- generate_curvilinear_2d_with_noise(n_points)
 
 # Generate the second curvilinear cluster in dimensions 3 and 4 with small noise in dimensions 1 and 2
-curv_3_4 <- generate_curvilinear_2d_with_noise(n_points, noise_dim1 = 0.05, noise_dim2 = 0.05) |>
-  as_tibble() |>
-  select(z_noise, w_noise, x, y)
+curv_3_4 <- generate_curvilinear_2d_with_noise(n_points) |>
+  select(x3, x4, x1, x2)
 
 names(curv_3_4) <- c("x1", "x2", "x3", "x4")
 
@@ -31,10 +36,12 @@ offset <- c(2, 2, 2, 2)  # Adjust these values to set the desired distance
 curv_3_4 <- sweep(curv_3_4, 2, offset, "+")
 
 # Combine the clusters to create the final dataset
-curvilinear_4d <- rbind(curv_1_2, curv_3_4)
+curvilinear_4d <- bind_rows(curv_1_2, curv_3_4)
 
 # View the first few rows
 head(curvilinear_4d)
 
 # Visualize using langevitour
 langevitour(curvilinear_4d)
+
+write_rds(curvilinear_4d, here::here("data/two_nonlinear_clusters/two_nonlinear_clusters_data.rds"))
