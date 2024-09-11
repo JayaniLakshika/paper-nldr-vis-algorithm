e2abdb764b88a00cb85a285a5eb2727396d40af7
JayaniLakshika <jayanilakshika76@gmail.com>
2024-09-11 22:27:21 +1000

    Need to update the code for two curvilinear cluster true model

--- scripts/two_curvilinear_clusters/3_gen_clusters_with_true_models.R
+++ scripts/two_curvilinear_clusters/3_gen_clusters_with_true_models.R
@@ -1,25 +1,30 @@
 library(dplyr)
 library(tidyr)
 
-# Function to generate a curvilinear pattern with noise in 2D and two noisy dimensions
-generate_curvilinear_2d_with_noise <- function(n, noise_dim1, noise_dim2) {
-  x <- runif(n, 0, 2)
-  y <- -(x^3 + runif(n, 0, 3)) + runif(n, 0, 0.5)
+# Function to generate a 2D curvilinear cluster with noise in the other dimensions
+generate_curvilinear_2d_with_noise <- function(n) {
+  x1 <- runif(n, 0, 2)
+  x2 <- -(x1^3 + runif(n, 0, 3)) + runif(n, 0, 0.5)
 
   # Add small noise to other dimensions
-  z_noise <- rnorm(n, mean = 0, sd = noise_dim1)
-  w_noise <- rnorm(n, mean = 0, sd = noise_dim2)
+  x3 <- sin(x1 * pi) + runif(n, -0.5, 0.5)
+  x4 <- rnorm(n, mean = 0, sd = 0.01)
 
-  return(cbind(x, y, z_noise, w_noise))
+  data <- tibble::tibble(x1 = x1,
+                         x2 = x2,
+                         x3 = x3,
+                         x4 = x4)
+
+  data
 }
 
 # Function to generate a curvilinear grid pattern in 2D
 generate_curvilinear_grid_2d <- function(n_grid_x, n_grid_y) {
-  x <- seq(0, 2, length.out = n_grid_x)
-  y <- seq(-0.5, 3, length.out = n_grid_y)
+  x1 <- seq(0, 2, length.out = n_grid_x)
+  x2 <- seq(-0.5, 3, length.out = n_grid_y)
 
   # Use expand.grid to create a grid of (x, y) pairs
-  curvilinear_grid <- expand.grid(x = x, y = y)
+  curvilinear_grid <- expand.grid(x1 = x1, x2 = x2)
 
   # Adjust y values according to a curvilinear pattern based on x
   curvilinear_grid$y <- -(curvilinear_grid$x^3 + curvilinear_grid$y)
@@ -29,26 +34,22 @@ generate_curvilinear_grid_2d <- function(n_grid_x, n_grid_y) {
 
 # Generate the curvilinear data with noise
 n_points <- 500
-curvilinear_data1 <- generate_curvilinear_2d_with_noise(n_points, noise_dim1 = 0.01, noise_dim2 = 0.01) %>%
-  as_tibble()
 
-names(curvilinear_data1) <- c("x", "y", "z_noise", "w_noise")
+# Generate the first curvilinear cluster in dimensions 1 and 2 with small noise in dimensions 3 and 4
+curv_1_2 <- generate_curvilinear_2d_with_noise(n_points)
 
-curvilinear_data2 <- generate_curvilinear_2d_with_noise(n_points, noise_dim1 = 0.01, noise_dim2 = 0.01) %>%
-  as_tibble()
+# Generate the second curvilinear cluster in dimensions 3 and 4 with small noise in dimensions 1 and 2
+curv_3_4 <- generate_curvilinear_2d_with_noise(n_points) |>
+  select(x3, x4, x1, x2)
 
-names(curvilinear_data2) <- c("x", "y", "z_noise", "w_noise")
-
-curvilinear_data2 <- curvilinear_data2 |>
-  select("z_noise", "w_noise", "x", "y")
-
-names(curvilinear_data2) <- c("x", "y", "z_noise", "w_noise")
+names(curv_3_4) <- c("x1", "x2", "x3", "x4")
 
 # Apply an offset to one of the clusters to create a distance between them
 offset <- c(2, 2, 2, 2)  # Adjust these values to set the desired distance
-curvilinear_data2 <- sweep(curvilinear_data2, 2, offset, "+")
+curv_3_4 <- sweep(curv_3_4, 2, offset, "+")
 
-curvilinear_data <- bind_rows(curvilinear_data1, curvilinear_data2)
+# Combine the clusters to create the final dataset
+curvilinear_4d <- bind_rows(curv_1_2, curv_3_4)
 
 curvilinear_data <- curvilinear_data |>
   mutate(type = "data")
