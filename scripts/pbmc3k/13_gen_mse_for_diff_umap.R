d46296c59a2f455bf1b6c0534f1c15510d2fce64
JayaniLakshika <jayanilakshika76@gmail.com>
2024-11-20 11:59:15 +1100

    Added results for different UMAP parameter choices

---
+++ scripts/pbmc3k/13_gen_mse_for_diff_umap.R
@@ -0,0 +1,574 @@
+library(readr)
+library(quollr)
+library(dplyr)
+
+training_data_pbmc <- read_rds("data/pbmc3k/pbmc_pca_50.rds")
+training_data_pbmc <- training_data_pbmc[, 1:9] |>
+  mutate(ID = 1:NROW(training_data_pbmc))
+
+## For umap
+umap_pbmc <- read_rds("data/pbmc3k/pbmc_scdeed_umap_n_neighbors_30_min_dist_0.3.rds")
+umap_pbmc <- as_tibble(umap_pbmc)
+names(umap_pbmc) <- c("UMAP1", "UMAP2")
+umap_pbmc <- umap_pbmc |>
+  mutate(ID = 1:NROW(umap_pbmc))
+
+pbmc_scaled_obj_umap <- gen_scaled_data(
+  data = umap_pbmc)
+umap_pbmc_scaled <- pbmc_scaled_obj_umap$scaled_nldr
+
+lim1 <- pbmc_scaled_obj_umap$lim1
+lim2 <- pbmc_scaled_obj_umap$lim2
+r2_umap <- diff(lim2)/diff(lim1)
+
+## To initialize number of bins along the x-axis
+bin1_vec_pbmc <- 2:53 #sqrt(NROW(training_data_pbmc)/r2_umap)
+
+error_pbmc_umap <- data.frame(matrix(nrow = 0, ncol = 0))
+
+for (xbins in bin1_vec_pbmc) {
+
+  hb_obj <- calc_bins_y(bin1 = xbins, r2 = r2_umap, q = 0.1)
+
+  bin2 <- hb_obj$bin2
+  a1 <- hb_obj$a1
+
+  pbmc_model <- fit_highd_model(
+    training_data = training_data_pbmc,
+    emb_df = umap_pbmc_scaled,
+    bin1 = xbins,
+    r2 = r2_umap,
+    q = 0.1,
+    is_bin_centroid = TRUE,
+    is_rm_lwd_hex = FALSE,
+    col_start_highd = "PC_"
+  )
+
+  df_bin_centroids_pbmc <- pbmc_model$df_bin_centroids
+  df_bin_pbmc <- pbmc_model$df_bin
+
+  ## Compute error
+  error_df <- glance(
+    df_bin_centroids = df_bin_centroids_pbmc,
+    df_bin = df_bin_pbmc,
+    training_data = training_data_pbmc,
+    newdata = NULL,
+    type_NLDR = "UMAP",
+    col_start = "PC_") |>
+    mutate(bin1 = xbins,
+           bin2 = bin2,
+           b = bin1 * bin2,
+           b_non_empty = NROW(df_bin_centroids_pbmc),
+           method = "UMAP_30_min_dist_0.3",
+           a1 = a1)
+
+  error_pbmc_umap <- bind_rows(error_pbmc_umap, error_df)
+
+}
+
+write_rds(error_pbmc_umap, "data/pbmc3k/error_scdeed_pbmc_umap_30_min_dist_0.3.rds")
+
+#################################
+
+## For umap
+umap_pbmc <- read_rds("data/pbmc3k/pbmc_scdeed_umap_n_neighbors_5_min_dist_0.01.rds")
+umap_pbmc <- as_tibble(umap_pbmc)
+names(umap_pbmc) <- c("UMAP1", "UMAP2")
+umap_pbmc <- umap_pbmc |>
+  mutate(ID = 1:NROW(umap_pbmc))
+
+pbmc_scaled_obj_umap <- gen_scaled_data(
+  data = umap_pbmc)
+umap_pbmc_scaled <- pbmc_scaled_obj_umap$scaled_nldr
+
+lim1 <- pbmc_scaled_obj_umap$lim1
+lim2 <- pbmc_scaled_obj_umap$lim2
+r2_umap <- diff(lim2)/diff(lim1)
+
+bin1_vec_pbmc <- 2:47 #sqrt(NROW(training_data_pbmc)/r2_umap)
+
+error_pbmc_umap <- data.frame(matrix(nrow = 0, ncol = 0))
+
+for (xbins in bin1_vec_pbmc) {
+
+  hb_obj <- calc_bins_y(bin1 = xbins, r2 = r2_umap, q = 0.1)
+
+  bin2 <- hb_obj$bin2
+  a1 <- hb_obj$a1
+
+  pbmc_model <- fit_highd_model(
+    training_data = training_data_pbmc,
+    emb_df = umap_pbmc_scaled,
+    bin1 = xbins,
+    r2 = r2_umap,
+    q = 0.1,
+    is_bin_centroid = TRUE,
+    is_rm_lwd_hex = FALSE,
+    col_start_highd = "PC_"
+  )
+
+  df_bin_centroids_pbmc <- pbmc_model$df_bin_centroids
+  df_bin_pbmc <- pbmc_model$df_bin
+
+  ## Compute error
+  error_df <- glance(
+    df_bin_centroids = df_bin_centroids_pbmc,
+    df_bin = df_bin_pbmc,
+    training_data = training_data_pbmc,
+    newdata = NULL,
+    type_NLDR = "UMAP",
+    col_start = "PC_") |>
+    mutate(bin1 = xbins,
+           bin2 = bin2,
+           b = bin1 * bin2,
+           b_non_empty = NROW(df_bin_centroids_pbmc),
+           method = "UMAP_5_min_dist_0.01",
+           a1 = a1)
+
+  error_pbmc_umap <- bind_rows(error_pbmc_umap, error_df)
+
+}
+
+write_rds(error_pbmc_umap, "data/pbmc3k/error_scdeed_pbmc_umap_5_min_dist_0.01.rds")
+
+#################################
+
+## For umap
+umap_pbmc <- read_rds("data/pbmc3k/pbmc_scdeed_umap_n_neighbors_15_min_dist_0.99.rds")
+umap_pbmc <- as_tibble(umap_pbmc)
+names(umap_pbmc) <- c("UMAP1", "UMAP2")
+umap_pbmc <- umap_pbmc |>
+  mutate(ID = 1:NROW(umap_pbmc))
+
+pbmc_scaled_obj_umap <- gen_scaled_data(
+  data = umap_pbmc)
+umap_pbmc_scaled <- pbmc_scaled_obj_umap$scaled_nldr
+
+lim1 <- pbmc_scaled_obj_umap$lim1
+lim2 <- pbmc_scaled_obj_umap$lim2
+r2_umap <- diff(lim2)/diff(lim1)
+
+bin1_vec_pbmc <- 2:59 #sqrt(NROW(training_data_pbmc)/r2_umap)
+
+error_pbmc_umap <- data.frame(matrix(nrow = 0, ncol = 0))
+
+for (xbins in bin1_vec_pbmc) {
+
+  hb_obj <- calc_bins_y(bin1 = xbins, r2 = r2_umap, q = 0.1)
+
+  bin2 <- hb_obj$bin2
+  a1 <- hb_obj$a1
+
+  pbmc_model <- fit_highd_model(
+    training_data = training_data_pbmc,
+    emb_df = umap_pbmc_scaled,
+    bin1 = xbins,
+    r2 = r2_umap,
+    q = 0.1,
+    is_bin_centroid = TRUE,
+    is_rm_lwd_hex = FALSE,
+    col_start_highd = "PC_"
+  )
+
+  df_bin_centroids_pbmc <- pbmc_model$df_bin_centroids
+  df_bin_pbmc <- pbmc_model$df_bin
+
+  ## Compute error
+  error_df <- glance(
+    df_bin_centroids = df_bin_centroids_pbmc,
+    df_bin = df_bin_pbmc,
+    training_data = training_data_pbmc,
+    newdata = NULL,
+    type_NLDR = "UMAP",
+    col_start = "PC_") |>
+    mutate(bin1 = xbins,
+           bin2 = bin2,
+           b = bin1 * bin2,
+           b_non_empty = NROW(df_bin_centroids_pbmc),
+           method = "UMAP_15_min_dist_0.99",
+           a1 = a1)
+
+  error_pbmc_umap <- bind_rows(error_pbmc_umap, error_df)
+
+}
+
+write_rds(error_pbmc_umap, "data/pbmc3k/error_scdeed_pbmc_umap_15_min_dist_0.99.rds")
+
+
+#################################
+
+## For umap
+umap_pbmc <- read_rds("data/pbmc3k/pbmc_scdeed_umap_n_neighbors_30_min_dist_0.99.rds")
+umap_pbmc <- as_tibble(umap_pbmc)
+names(umap_pbmc) <- c("UMAP1", "UMAP2")
+umap_pbmc <- umap_pbmc |>
+  mutate(ID = 1:NROW(umap_pbmc))
+
+pbmc_scaled_obj_umap <- gen_scaled_data(
+  data = umap_pbmc)
+umap_pbmc_scaled <- pbmc_scaled_obj_umap$scaled_nldr
+
+lim1 <- pbmc_scaled_obj_umap$lim1
+lim2 <- pbmc_scaled_obj_umap$lim2
+r2_umap <- diff(lim2)/diff(lim1)
+
+bin1_vec_pbmc <- 2:60 #sqrt(NROW(training_data_pbmc)/r2_umap)
+
+error_pbmc_umap <- data.frame(matrix(nrow = 0, ncol = 0))
+
+for (xbins in bin1_vec_pbmc) {
+
+  hb_obj <- calc_bins_y(bin1 = xbins, r2 = r2_umap, q = 0.1)
+
+  bin2 <- hb_obj$bin2
+  a1 <- hb_obj$a1
+
+  pbmc_model <- fit_highd_model(
+    training_data = training_data_pbmc,
+    emb_df = umap_pbmc_scaled,
+    bin1 = xbins,
+    r2 = r2_umap,
+    q = 0.1,
+    is_bin_centroid = TRUE,
+    is_rm_lwd_hex = FALSE,
+    col_start_highd = "PC_"
+  )
+
+  df_bin_centroids_pbmc <- pbmc_model$df_bin_centroids
+  df_bin_pbmc <- pbmc_model$df_bin
+
+  ## Compute error
+  error_df <- glance(
+    df_bin_centroids = df_bin_centroids_pbmc,
+    df_bin = df_bin_pbmc,
+    training_data = training_data_pbmc,
+    newdata = NULL,
+    type_NLDR = "UMAP",
+    col_start = "PC_") |>
+    mutate(bin1 = xbins,
+           bin2 = bin2,
+           b = bin1 * bin2,
+           b_non_empty = NROW(df_bin_centroids_pbmc),
+           method = "UMAP_30_min_dist_0.99",
+           a1 = a1)
+
+  error_pbmc_umap <- bind_rows(error_pbmc_umap, error_df)
+
+}
+
+write_rds(error_pbmc_umap, "data/pbmc3k/error_scdeed_pbmc_umap_30_min_dist_0.99.rds")
+
+#################################
+
+## For umap
+umap_pbmc <- read_rds("data/pbmc3k/pbmc_scdeed_umap_n_neighbors_5_min_dist_0.99.rds")
+umap_pbmc <- as_tibble(umap_pbmc)
+names(umap_pbmc) <- c("UMAP1", "UMAP2")
+umap_pbmc <- umap_pbmc |>
+  mutate(ID = 1:NROW(umap_pbmc))
+
+pbmc_scaled_obj_umap <- gen_scaled_data(
+  data = umap_pbmc)
+umap_pbmc_scaled <- pbmc_scaled_obj_umap$scaled_nldr
+
+lim1 <- pbmc_scaled_obj_umap$lim1
+lim2 <- pbmc_scaled_obj_umap$lim2
+r2_umap <- diff(lim2)/diff(lim1)
+
+bin1_vec_pbmc <- 2:53 #sqrt(NROW(training_data_pbmc)/r2_umap)
+
+error_pbmc_umap <- data.frame(matrix(nrow = 0, ncol = 0))
+
+for (xbins in bin1_vec_pbmc) {
+
+  hb_obj <- calc_bins_y(bin1 = xbins, r2 = r2_umap, q = 0.1)
+
+  bin2 <- hb_obj$bin2
+  a1 <- hb_obj$a1
+
+  pbmc_model <- fit_highd_model(
+    training_data = training_data_pbmc,
+    emb_df = umap_pbmc_scaled,
+    bin1 = xbins,
+    r2 = r2_umap,
+    q = 0.1,
+    is_bin_centroid = TRUE,
+    is_rm_lwd_hex = FALSE,
+    col_start_highd = "PC_"
+  )
+
+  df_bin_centroids_pbmc <- pbmc_model$df_bin_centroids
+  df_bin_pbmc <- pbmc_model$df_bin
+
+  ## Compute error
+  error_df <- glance(
+    df_bin_centroids = df_bin_centroids_pbmc,
+    df_bin = df_bin_pbmc,
+    training_data = training_data_pbmc,
+    newdata = NULL,
+    type_NLDR = "UMAP",
+    col_start = "PC_") |>
+    mutate(bin1 = xbins,
+           bin2 = bin2,
+           b = bin1 * bin2,
+           b_non_empty = NROW(df_bin_centroids_pbmc),
+           method = "UMAP_5_min_dist_0.99",
+           a1 = a1)
+
+  error_pbmc_umap <- bind_rows(error_pbmc_umap, error_df)
+
+}
+
+write_rds(error_pbmc_umap, "data/pbmc3k/error_scdeed_pbmc_umap_5_min_dist_0.99.rds")
+
+#################################
+
+## For umap
+umap_pbmc <- read_rds("data/pbmc3k/pbmc_scdeed_umap_n_neighbors_15_min_dist_0.3.rds")
+umap_pbmc <- as_tibble(umap_pbmc)
+names(umap_pbmc) <- c("UMAP1", "UMAP2")
+umap_pbmc <- umap_pbmc |>
+  mutate(ID = 1:NROW(umap_pbmc))
+
+pbmc_scaled_obj_umap <- gen_scaled_data(
+  data = umap_pbmc)
+umap_pbmc_scaled <- pbmc_scaled_obj_umap$scaled_nldr
+
+lim1 <- pbmc_scaled_obj_umap$lim1
+lim2 <- pbmc_scaled_obj_umap$lim2
+r2_umap <- diff(lim2)/diff(lim1)
+
+bin1_vec_pbmc <- 2:56 #sqrt(NROW(training_data_pbmc)/r2_umap)
+
+error_pbmc_umap <- data.frame(matrix(nrow = 0, ncol = 0))
+
+for (xbins in bin1_vec_pbmc) {
+
+  hb_obj <- calc_bins_y(bin1 = xbins, r2 = r2_umap, q = 0.1)
+
+  bin2 <- hb_obj$bin2
+  a1 <- hb_obj$a1
+
+  pbmc_model <- fit_highd_model(
+    training_data = training_data_pbmc,
+    emb_df = umap_pbmc_scaled,
+    bin1 = xbins,
+    r2 = r2_umap,
+    q = 0.1,
+    is_bin_centroid = TRUE,
+    is_rm_lwd_hex = FALSE,
+    col_start_highd = "PC_"
+  )
+
+  df_bin_centroids_pbmc <- pbmc_model$df_bin_centroids
+  df_bin_pbmc <- pbmc_model$df_bin
+
+  ## Compute error
+  error_df <- glance(
+    df_bin_centroids = df_bin_centroids_pbmc,
+    df_bin = df_bin_pbmc,
+    training_data = training_data_pbmc,
+    newdata = NULL,
+    type_NLDR = "UMAP",
+    col_start = "PC_") |>
+    mutate(bin1 = xbins,
+           bin2 = bin2,
+           b = bin1 * bin2,
+           b_non_empty = NROW(df_bin_centroids_pbmc),
+           method = "UMAP_15_min_dist_0.3",
+           a1 = a1)
+
+  error_pbmc_umap <- bind_rows(error_pbmc_umap, error_df)
+
+}
+
+write_rds(error_pbmc_umap, "data/pbmc3k/error_scdeed_pbmc_umap_15_min_dist_0.3.rds")
+
+#################################
+
+## For umap
+umap_pbmc <- read_rds("data/pbmc3k/pbmc_scdeed_umap_n_neighbors_5_min_dist_0.3.rds")
+umap_pbmc <- as_tibble(umap_pbmc)
+names(umap_pbmc) <- c("UMAP1", "UMAP2")
+umap_pbmc <- umap_pbmc |>
+  mutate(ID = 1:NROW(umap_pbmc))
+
+pbmc_scaled_obj_umap <- gen_scaled_data(
+  data = umap_pbmc)
+umap_pbmc_scaled <- pbmc_scaled_obj_umap$scaled_nldr
+
+lim1 <- pbmc_scaled_obj_umap$lim1
+lim2 <- pbmc_scaled_obj_umap$lim2
+r2_umap <- diff(lim2)/diff(lim1)
+
+bin1_vec_pbmc <- 2:54 #sqrt(NROW(training_data_pbmc)/r2_umap)
+
+error_pbmc_umap <- data.frame(matrix(nrow = 0, ncol = 0))
+
+for (xbins in bin1_vec_pbmc) {
+
+  hb_obj <- calc_bins_y(bin1 = xbins, r2 = r2_umap, q = 0.1)
+
+  bin2 <- hb_obj$bin2
+  a1 <- hb_obj$a1
+
+  pbmc_model <- fit_highd_model(
+    training_data = training_data_pbmc,
+    emb_df = umap_pbmc_scaled,
+    bin1 = xbins,
+    r2 = r2_umap,
+    q = 0.1,
+    is_bin_centroid = TRUE,
+    is_rm_lwd_hex = FALSE,
+    col_start_highd = "PC_"
+  )
+
+  df_bin_centroids_pbmc <- pbmc_model$df_bin_centroids
+  df_bin_pbmc <- pbmc_model$df_bin
+
+  ## Compute error
+  error_df <- glance(
+    df_bin_centroids = df_bin_centroids_pbmc,
+    df_bin = df_bin_pbmc,
+    training_data = training_data_pbmc,
+    newdata = NULL,
+    type_NLDR = "UMAP",
+    col_start = "PC_") |>
+    mutate(bin1 = xbins,
+           bin2 = bin2,
+           b = bin1 * bin2,
+           b_non_empty = NROW(df_bin_centroids_pbmc),
+           method = "UMAP_5_min_dist_0.3",
+           a1 = a1)
+
+  error_pbmc_umap <- bind_rows(error_pbmc_umap, error_df)
+
+}
+
+write_rds(error_pbmc_umap, "data/pbmc3k/error_scdeed_pbmc_umap_5_min_dist_0.3.rds")
+
+#################################
+
+## For umap
+umap_pbmc <- read_rds("data/pbmc3k/pbmc_scdeed_umap_n_neighbors_30_min_dist_0.01.rds")
+umap_pbmc <- as_tibble(umap_pbmc)
+names(umap_pbmc) <- c("UMAP1", "UMAP2")
+umap_pbmc <- umap_pbmc |>
+  mutate(ID = 1:NROW(umap_pbmc))
+
+pbmc_scaled_obj_umap <- gen_scaled_data(
+  data = umap_pbmc)
+umap_pbmc_scaled <- pbmc_scaled_obj_umap$scaled_nldr
+
+lim1 <- pbmc_scaled_obj_umap$lim1
+lim2 <- pbmc_scaled_obj_umap$lim2
+r2_umap <- diff(lim2)/diff(lim1)
+
+bin1_vec_pbmc <- 2:56 #sqrt(NROW(training_data_pbmc)/r2_umap)
+
+error_pbmc_umap <- data.frame(matrix(nrow = 0, ncol = 0))
+
+for (xbins in bin1_vec_pbmc) {
+
+  hb_obj <- calc_bins_y(bin1 = xbins, r2 = r2_umap, q = 0.1)
+
+  bin2 <- hb_obj$bin2
+  a1 <- hb_obj$a1
+
+  pbmc_model <- fit_highd_model(
+    training_data = training_data_pbmc,
+    emb_df = umap_pbmc_scaled,
+    bin1 = xbins,
+    r2 = r2_umap,
+    q = 0.1,
+    is_bin_centroid = TRUE,
+    is_rm_lwd_hex = FALSE,
+    col_start_highd = "PC_"
+  )
+
+  df_bin_centroids_pbmc <- pbmc_model$df_bin_centroids
+  df_bin_pbmc <- pbmc_model$df_bin
+
+  ## Compute error
+  error_df <- glance(
+    df_bin_centroids = df_bin_centroids_pbmc,
+    df_bin = df_bin_pbmc,
+    training_data = training_data_pbmc,
+    newdata = NULL,
+    type_NLDR = "UMAP",
+    col_start = "PC_") |>
+    mutate(bin1 = xbins,
+           bin2 = bin2,
+           b = bin1 * bin2,
+           b_non_empty = NROW(df_bin_centroids_pbmc),
+           method = "UMAP_30_min_dist_0.01",
+           a1 = a1)
+
+  error_pbmc_umap <- bind_rows(error_pbmc_umap, error_df)
+
+}
+
+write_rds(error_pbmc_umap, "data/pbmc3k/error_scdeed_pbmc_umap_30_min_dist_0.01.rds")
+
+#################################
+
+## For umap
+umap_pbmc <- read_rds("data/pbmc3k/pbmc_scdeed_umap_n_neighbors_15_min_dist_0.01.rds")
+umap_pbmc <- as_tibble(umap_pbmc)
+names(umap_pbmc) <- c("UMAP1", "UMAP2")
+umap_pbmc <- umap_pbmc |>
+  mutate(ID = 1:NROW(umap_pbmc))
+
+pbmc_scaled_obj_umap <- gen_scaled_data(
+  data = umap_pbmc)
+umap_pbmc_scaled <- pbmc_scaled_obj_umap$scaled_nldr
+
+lim1 <- pbmc_scaled_obj_umap$lim1
+lim2 <- pbmc_scaled_obj_umap$lim2
+r2_umap <- diff(lim2)/diff(lim1)
+
+bin1_vec_pbmc <- 2:58 #sqrt(NROW(training_data_pbmc)/r2_umap)
+
+error_pbmc_umap <- data.frame(matrix(nrow = 0, ncol = 0))
+
+for (xbins in bin1_vec_pbmc) {
+
+  hb_obj <- calc_bins_y(bin1 = xbins, r2 = r2_umap, q = 0.1)
+
+  bin2 <- hb_obj$bin2
+  a1 <- hb_obj$a1
+
+  pbmc_model <- fit_highd_model(
+    training_data = training_data_pbmc,
+    emb_df = umap_pbmc_scaled,
+    bin1 = xbins,
+    r2 = r2_umap,
+    q = 0.1,
+    is_bin_centroid = TRUE,
+    is_rm_lwd_hex = FALSE,
+    col_start_highd = "PC_"
+  )
+
+  df_bin_centroids_pbmc <- pbmc_model$df_bin_centroids
+  df_bin_pbmc <- pbmc_model$df_bin
+
+  ## Compute error
+  error_df <- glance(
+    df_bin_centroids = df_bin_centroids_pbmc,
+    df_bin = df_bin_pbmc,
+    training_data = training_data_pbmc,
+    newdata = NULL,
+    type_NLDR = "UMAP",
+    col_start = "PC_") |>
+    mutate(bin1 = xbins,
+           bin2 = bin2,
+           b = bin1 * bin2,
+           b_non_empty = NROW(df_bin_centroids_pbmc),
+           method = "UMAP_15_min_dist_0.01",
+           a1 = a1)
+
+  error_pbmc_umap <- bind_rows(error_pbmc_umap, error_df)
+
+}
+
+write_rds(error_pbmc_umap, "data/pbmc3k/error_scdeed_pbmc_umap_15_min_dist_0.01.rds")
