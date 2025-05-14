a480ec3b3d7a0ad0127c90cee174dff1680adec6
JayaniLakshika <jayanilakshika76@gmail.com>
2025-05-15 09:33:08 +1000

    Started updating the appendix

--- scripts/pbmc3k/13_gen_mse_for_diff_tsne_scDEED.R
+++ scripts/pbmc3k/13_gen_mse_for_diff_tsne_scDEED.R
@@ -4,11 +4,11 @@ library(readr)
 library(quollr)
 library(dplyr)
 
-training_data_pbmc <- read_rds("data/pbmc3k/pbmc_pca_50_scdeed.rds")
-training_data_pbmc <- training_data_pbmc[, 1:9] |>
-  mutate(ID = 1:NROW(training_data_pbmc))
+data_pbmc <- read_rds("data/pbmc3k/pbmc_pca_50_scdeed.rds")
+data_pbmc <- data_pbmc[, 1:9] |>
+  mutate(ID = 1:NROW(data_pbmc))
 
-names(training_data_pbmc) <- append(paste0("x", 1:9), "ID")
+names(data_pbmc) <- append(paste0("x", 1:9), "ID")
 
 ## For tsne
 tsne_pbmc <- read_rds("data/pbmc3k/pbmc_scdeed_tsne_perplexity_30.rds")
@@ -17,53 +17,8 @@ names(tsne_pbmc) <- c("emb1", "emb2")
 tsne_pbmc <- tsne_pbmc |>
   mutate(ID = 1:NROW(tsne_pbmc))
 
-pbmc_scaled_obj_tsne <- gen_scaled_data(
-  data = tsne_pbmc)
-tsne_pbmc_scaled <- pbmc_scaled_obj_tsne$scaled_nldr
-
-lim1 <- pbmc_scaled_obj_tsne$lim1
-lim2 <- pbmc_scaled_obj_tsne$lim2
-r2_tsne <- diff(lim2)/diff(lim1)
-
-## To initialize number of bins along the x-axis
-bin1_vec_pbmc <- 2:78 #sqrt(NROW(training_data_pbmc)/r2_tsne)
-
-error_pbmc_tsne <- data.frame(matrix(nrow = 0, ncol = 0))
-
-for (xbins in bin1_vec_pbmc) {
-
-  hb_obj <- calc_bins_y(bin1 = xbins, r2 = r2_tsne, q = 0.1)
-
-  bin2 <- hb_obj$bin2
-  a1 <- hb_obj$a1
-
-  pbmc_model <- fit_highd_model(
-    highd_data = training_data_pbmc,
-    nldr_data = tsne_pbmc_scaled,
-    bin1 = xbins,
-    r2 = r2_tsne,
-    q = 0.1,
-    is_bin_centroid = TRUE
-  )
-
-  df_bin_centroids_pbmc <- pbmc_model$df_bin_centroids
-  df_bin_pbmc <- pbmc_model$df_bin
-
-  ## Compute error
-  error_df <- glance(
-    model_2d = df_bin_centroids_pbmc,
-    model_highd = df_bin_pbmc,
-    highd_data = training_data_pbmc) |>
-    mutate(bin1 = xbins,
-           bin2 = bin2,
-           b = bin1 * bin2,
-           b_non_empty = NROW(df_bin_centroids_pbmc),
-           method = "tsne_perplexity_30",
-           a1 = a1)
-
-  error_pbmc_tsne <- bind_rows(error_pbmc_tsne, error_df)
-
-}
+error_pbmc_tsne <- gen_diffbin1_errors(highd_data = data_pbmc, nldr_data = tsne_pbmc) |>
+  dplyr::mutate(method = "tsne_perplexity_30")
 
 write_rds(error_pbmc_tsne, "data/pbmc3k/error_scdeed_pbmc_tsne_perplexity_30.rds")
 
@@ -76,52 +31,8 @@ names(tsne_pbmc) <- c("emb1", "emb2")
 tsne_pbmc <- tsne_pbmc |>
   mutate(ID = 1:NROW(tsne_pbmc))
 
-pbmc_scaled_obj_tsne <- gen_scaled_data(
-  data = tsne_pbmc)
-tsne_pbmc_scaled <- pbmc_scaled_obj_tsne$scaled_nldr
-
-lim1 <- pbmc_scaled_obj_tsne$lim1
-lim2 <- pbmc_scaled_obj_tsne$lim2
-r2_tsne <- diff(lim2)/diff(lim1)
-
-bin1_vec_pbmc <- 2:87 #sqrt(NROW(training_data_pbmc)/r2_tsne)
-
-error_pbmc_tsne <- data.frame(matrix(nrow = 0, ncol = 0))
-
-for (xbins in bin1_vec_pbmc) {
-
-  hb_obj <- calc_bins_y(bin1 = xbins, r2 = r2_tsne, q = 0.1)
-
-  bin2 <- hb_obj$bin2
-  a1 <- hb_obj$a1
-
-  pbmc_model <- fit_highd_model(
-    highd_data = training_data_pbmc,
-    nldr_data = tsne_pbmc_scaled,
-    bin1 = xbins,
-    r2 = r2_tsne,
-    q = 0.1,
-    is_bin_centroid = TRUE
-  )
-
-  df_bin_centroids_pbmc <- pbmc_model$df_bin_centroids
-  df_bin_pbmc <- pbmc_model$df_bin
-
-  ## Compute error
-  error_df <- glance(
-    model_2d = df_bin_centroids_pbmc,
-    model_highd = df_bin_pbmc,
-    highd_data = training_data_pbmc) |>
-    mutate(bin1 = xbins,
-           bin2 = bin2,
-           b = bin1 * bin2,
-           b_non_empty = NROW(df_bin_centroids_pbmc),
-           method = "tsne_perplexity_320",
-           a1 = a1)
-
-  error_pbmc_tsne <- bind_rows(error_pbmc_tsne, error_df)
-
-}
+error_pbmc_tsne <- gen_diffbin1_errors(highd_data = data_pbmc, nldr_data = tsne_pbmc) |>
+  dplyr::mutate(method = "tsne_perplexity_320")
 
 write_rds(error_pbmc_tsne, "data/pbmc3k/error_scdeed_pbmc_tsne_perplexity_320.rds")
 
--- scripts/pbmc3k/13_gen_mse_for_diff_umap_scdeed.R
+++ scripts/pbmc3k/13_gen_mse_for_diff_umap_scdeed.R
@@ -3,11 +3,11 @@ library(readr)
 library(quollr)
 library(dplyr)
 
-training_data_pbmc <- read_rds("data/pbmc3k/pbmc_pca_50_scdeed.rds")
-training_data_pbmc <- training_data_pbmc[, 1:9] |>
-  mutate(ID = 1:NROW(training_data_pbmc))
+data_pbmc <- read_rds("data/pbmc3k/pbmc_pca_50_scdeed.rds")
+data_pbmc <- data_pbmc[, 1:9] |>
+  mutate(ID = 1:NROW(data_pbmc))
 
-names(training_data_pbmc) <- append(paste0("x", 1:9), "ID")
+names(data_pbmc) <- append(paste0("x", 1:9), "ID")
 
 ## For umap
 umap_pbmc <- read_rds("data/pbmc3k/pbmc_scdeed_umap_n_neighbors_30_min_dist_0.3.rds")
@@ -16,53 +16,8 @@ names(umap_pbmc) <- c("emb1", "emb2")
 umap_pbmc <- umap_pbmc |>
   mutate(ID = 1:NROW(umap_pbmc))
 
-pbmc_scaled_obj_umap <- gen_scaled_data(
-  data = umap_pbmc)
-umap_pbmc_scaled <- pbmc_scaled_obj_umap$scaled_nldr
-
-lim1 <- pbmc_scaled_obj_umap$lim1
-lim2 <- pbmc_scaled_obj_umap$lim2
-r2_umap <- diff(lim2)/diff(lim1)
-
-## To initialize number of bins along the x-axis
-bin1_vec_pbmc <- 2:84 #sqrt(NROW(training_data_pbmc)/r2_umap)
-
-error_pbmc_umap <- data.frame(matrix(nrow = 0, ncol = 0))
-
-for (xbins in bin1_vec_pbmc) {
-
-  hb_obj <- calc_bins_y(bin1 = xbins, r2 = r2_umap, q = 0.1)
-
-  bin2 <- hb_obj$bin2
-  a1 <- hb_obj$a1
-
-  pbmc_model <- fit_highd_model(
-    highd_data = training_data_pbmc,
-    nldr_data = umap_pbmc_scaled,
-    bin1 = xbins,
-    r2 = r2_umap,
-    q = 0.1,
-    is_bin_centroid = TRUE
-  )
-
-  df_bin_centroids_pbmc <- pbmc_model$df_bin_centroids
-  df_bin_pbmc <- pbmc_model$df_bin
-
-  ## Compute error
-  error_df <- glance(
-    model_2d = df_bin_centroids_pbmc,
-    model_highd = df_bin_pbmc,
-    highd_data = training_data_pbmc) |>
-    mutate(bin1 = xbins,
-           bin2 = bin2,
-           b = bin1 * bin2,
-           b_non_empty = NROW(df_bin_centroids_pbmc),
-           method = "UMAP_30_min_dist_0.3",
-           a1 = a1)
-
-  error_pbmc_umap <- bind_rows(error_pbmc_umap, error_df)
-
-}
+error_pbmc_umap <- gen_diffbin1_errors(highd_data = data_pbmc, nldr_data = umap_pbmc) |>
+  dplyr::mutate(method = "UMAP_30_min_dist_0.3")
 
 write_rds(error_pbmc_umap, "data/pbmc3k/error_scdeed_pbmc_umap_30_min_dist_0.3.rds")
 
@@ -75,52 +30,8 @@ names(umap_pbmc) <- c("emb1", "emb2")
 umap_pbmc <- umap_pbmc |>
   mutate(ID = 1:NROW(umap_pbmc))
 
-pbmc_scaled_obj_umap <- gen_scaled_data(
-  data = umap_pbmc)
-umap_pbmc_scaled <- pbmc_scaled_obj_umap$scaled_nldr
-
-lim1 <- pbmc_scaled_obj_umap$lim1
-lim2 <- pbmc_scaled_obj_umap$lim2
-r2_umap <- diff(lim2)/diff(lim1)
-
-bin1_vec_pbmc <- 2:69 #sqrt(NROW(training_data_pbmc)/r2_umap)
-
-error_pbmc_umap <- data.frame(matrix(nrow = 0, ncol = 0))
-
-for (xbins in bin1_vec_pbmc) {
-
-  hb_obj <- calc_bins_y(bin1 = xbins, r2 = r2_umap, q = 0.1)
-
-  bin2 <- hb_obj$bin2
-  a1 <- hb_obj$a1
-
-  pbmc_model <- fit_highd_model(
-    highd_data = training_data_pbmc,
-    nldr_data = umap_pbmc_scaled,
-    bin1 = xbins,
-    r2 = r2_umap,
-    q = 0.1,
-    is_bin_centroid = TRUE
-  )
-
-  df_bin_centroids_pbmc <- pbmc_model$df_bin_centroids
-  df_bin_pbmc <- pbmc_model$df_bin
-
-  ## Compute error
-  error_df <- glance(
-    model_2d = df_bin_centroids_pbmc,
-    model_highd = df_bin_pbmc,
-    highd_data = training_data_pbmc) |>
-    mutate(bin1 = xbins,
-           bin2 = bin2,
-           b = bin1 * bin2,
-           b_non_empty = NROW(df_bin_centroids_pbmc),
-           method = "UMAP_80_min_dist_0.5",
-           a1 = a1)
-
-  error_pbmc_umap <- bind_rows(error_pbmc_umap, error_df)
-
-}
+error_pbmc_umap <- gen_diffbin1_errors(highd_data = data_pbmc, nldr_data = umap_pbmc) |>
+  dplyr::mutate(method = "UMAP_80_min_dist_0.5")
 
 write_rds(error_pbmc_umap, "data/pbmc3k/error_scdeed_pbmc_umap_80_min_dist_0.5.rds")
 
