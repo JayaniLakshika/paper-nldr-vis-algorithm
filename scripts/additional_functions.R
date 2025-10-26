fc09e2bea764ab86d3eac1cf33d04d0bbd9e341f
dicook <visnut@gmail.com>
2025-10-26 16:33:32 +1100

    finished editing paper, now need to respond to reviewer 2

--- scripts/additional_functions.R
+++ scripts/additional_functions.R
@@ -77,8 +77,9 @@ plot_rmse_dh <- function(error_df) {
              colour = method)) +
     geom_point(size = 0.8) +
     geom_line(linewidth = 0.3) +
+    scale_x_log10() +
     ylab("RMSE") +
-    xlab(expression(paste("bin density (", d[h], ")"))) +
+    xlab(expression(paste("average bin density (", d[h], ")"))) +
     theme_minimal() +
     theme(panel.border = element_rect(fill = 'transparent'),
           plot.title = element_text(size = 12, hjust = 0.5, vjust = -0.5),
