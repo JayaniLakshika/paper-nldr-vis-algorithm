10737f98b98972add12de698d4c41b2880dfb942
JayaniLakshika <jayanilakshika76@gmail.com>
2026-04-07 10:11:40 +1000

    Did the prediction

--- scripts/two_nonlinear/09_gen_model_prediction_with_umap_and_our_approach.R
+++ scripts/two_nonlinear/09_gen_model_prediction_with_umap_and_our_approach.R
@@ -12,63 +12,85 @@ set.seed(20240110)
 clr_choice <- "#0077A3"
 
 ## Data
-data_two_curvy <- read_rds("data/two_nonlinear/two_non_linear_diff_shaped_close_clusters_data.rds") |>
-  mutate(ID = row_number())
+UMAP_data <- read_rds("data/two_nonlinear/two_non_linear_diff_shaped_close_clusters_umap_n-neigbors_15_min-dist_0.1.rds")
 
-## To generate cluster column to make the split
-data_two_curvy <- data_two_curvy |>
+UMAP_data <- UMAP_data |>
+  mutate(ID = 1:NROW(UMAP_data)) |>
   mutate(cluster = if_else(ID <= 1000, "cluster1", "cluster2"))
-
-## To split the data
-
-split <- initial_split(data_two_curvy, prop = 0.7, strata = cluster)
-training_data_two_curvy <- training(split)
-testing_data_two_curvy <- testing(split)
-
-write_rds(testing_data_two_curvy, file = "data/two_nonlinear/testing_two_non_linear_diff_shaped_close_clusters.rds")
 
 ################################################################################
 
-## UMAP
+## To split the data
 
-n_neighbors <- 15
-min_dist <- 0.1
+split <- initial_split(UMAP_data, prop = 0.7, strata = cluster)
+training_umap_two_curvy <- training(split)
+test_umap_two_curvy <- testing(split)
 
-# Create a config list with the desired parameters
-umap_config <- umap.defaults
-umap_config$n_neighbors <- n_neighbors      # Set the number of neighbors
-umap_config$n_components <- 2    # Set the number of output dimensions (typically 2 or 3)
-umap_config$min_dist <- min_dist
+write_rds(training_umap_two_curvy, file = "data/two_nonlinear/training_umap_two_non_linear_diff_shaped_close_clusters.rds")
+write_rds(test_umap_two_curvy, file = "data/two_nonlinear/test_umap_two_non_linear_diff_shaped_close_clusters.rds")
 
-UMAP_fit <- umap(training_data_two_curvy[, 1:7], config = umap_config)
+## Select training and test data
 
-UMAP_data <- UMAP_fit$layout |>
-  as_tibble()
+data_two_curvy <- read_rds(here::here("data/two_nonlinear/two_non_linear_diff_shaped_close_clusters_data.rds"))
+data_two_curvy <- data_two_curvy |>
+  mutate(ID = 1:NROW(data_two_curvy))
 
-names(UMAP_data) <- c("UMAP1", "UMAP2")
 
-## Run only once
-write_rds(UMAP_data, file = paste0("data/two_nonlinear/test_two_non_linear_diff_shaped_close_clusters_umap_n-neigbors_", n_neighbors, "_min-dist_", min_dist, ".rds"))
+training_data_two_curvy <- data_two_curvy |>
+  filter(ID %in% training_umap_two_curvy$ID)
 
-## Predict for true model
+test_data_two_curvy <- data_two_curvy |>
+  filter(ID %in% test_umap_two_curvy$ID)
 
-predict_UMAP_df <- predict(UMAP_fit, testing_data_two_curvy[,1:7]) |>
-  as_tibble()
+write_rds(training_data_two_curvy, file = "data/two_nonlinear/training_two_non_linear_diff_shaped_close_clusters.rds")
+write_rds(test_data_two_curvy, file = "data/two_nonlinear/test_two_non_linear_diff_shaped_close_clusters.rds")
 
-names(predict_UMAP_df) <- c("UMAP1", "UMAP2")
+# ## To generate cluster column to make the split
+# data_two_curvy <- data_two_curvy |>
+#   mutate(cluster = if_else(ID <= 1000, "cluster1", "cluster2"))
+#
+# ##############################################################################
+#
+# ## UMAP
+#
+# n_neighbors <- 15
+# min_dist <- 0.1
+#
+# # Create a config list with the desired parameters
+# umap_config <- umap.defaults
+# umap_config$n_neighbors <- n_neighbors      # Set the number of neighbors
+# umap_config$n_components <- 2    # Set the number of output dimensions (typically 2 or 3)
+# umap_config$min_dist <- min_dist
+#
+# UMAP_fit <- umap(data_two_curvy[, 1:7], config = umap_config)
+#
+# UMAP_data <- UMAP_fit$layout |>
+#   as_tibble()
+#
+# names(UMAP_data) <- c("emb1", "emb2")
+#
+# ## Run only once
+# write_rds(UMAP_data, file = paste0("data/two_nonlinear/test_two_non_linear_diff_shaped_close_clusters_umap_n-neigbors_", n_neighbors, "_min-dist_", min_dist, ".rds"))
 
-## Run only once
-write_rds(predict_UMAP_df, file = "data/two_nonlinear/test_two_non_linear_diff_shaped_close_clusters_umap_predict.rds")
+# ## Predict for true model
+#
+# predict_UMAP_df <- predict(UMAP_fit, testing_data_two_curvy[,1:7]) |>
+#   as_tibble()
+#
+# names(predict_UMAP_df) <- c("UMAP1", "UMAP2")
+#
+# ## Run only once
+# write_rds(predict_UMAP_df, file = "data/two_nonlinear/test_two_non_linear_diff_shaped_close_clusters_umap_predict.rds")
 
 ################################################################################
 
 ## Fit the model
 
-num_bins_x_two_curvy <- 24
+num_bins_x_two_curvy <- 30
 
 algo_obj_two_curvy <- fit_highd_model(
   highd_data = training_data_two_curvy[, 1:8],
-  nldr_data = UMAP_data,
+  nldr_data = training_umap_two_curvy[, 1:3],
   b1 = num_bins_x_two_curvy,
   q = 0.1,
   hd_thresh = 0)
@@ -88,39 +110,3 @@ predict_df <- predict_emb(highd_data = testing_data_two_curvy[,1:8],
 
 ## Run only once
 write_rds(predict_df, file = "data/two_nonlinear/test_two_non_linear_diff_shaped_close_clusters_highd_vis_predict.rds")
-
-################################################################################
-
-## First projection of test data
-projection <- cbind(
-  c(0.09800,0.01534,0.01887,0.00252,0.01737,-0.06895,-0.00886),
-  c(-0.05248,-0.05845,0.06057,-0.00352,0.01697,-0.06938,0.01953))
-
-highd_data <- dplyr::select(testing_data_two_curvy[,1:7], tidyselect::starts_with("x"))
-projected <- as.matrix(testing_data_two_curvy[,1:7]) %*% projection
-projected_df <- dplyr::mutate(dplyr::rename(tibble::as_tibble(projected,
-                                                              .name_repair = "unique"),
-                                            c(proj1 = "...1", proj2 = "...2")),
-                              ID = dplyr::row_number())
-
-axis_param = list(limits = 0.8,
-                  axis_scaled = 5,
-                  axis_pos_x = -0.95,
-                  axis_pos_y = -0.95,
-                  threshold = 0.042)
-
-limits <- axis_param$limits
-axis_scaled <- axis_param$axis_scaled
-axis_pos_x <- axis_param$axis_pos_x
-axis_pos_y <- axis_param$axis_pos_y
-threshold <- axis_param$threshold
-axes_obj <- gen_axes(proj = projection * axis_scaled, limits = limits,
-                     axis_pos_x = axis_pos_x, axis_pos_y = axis_pos_y, axis_labels = names(highd_data),
-                     threshold = threshold)
-axes <- axes_obj$axes
-circle <- axes_obj$circle
-
-proj_obj1 <- list(projected_df = projected_df,
-     axes = axes, circle = circle)
-
-write_rds(proj_obj1, "data/two_nonlinear/test_two_nonlinear_proj_obj1.rds")
